ea1081e8840b90a02672c3681335e98715db38ed
Simon Ball <simonball245@gmail.com>
2024-04-24 17:41:58 +0200

    Add a form to create a new birthday (in progress)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,7 +16,7 @@ import { NewBirthdayComponent } from './components/new-birthday/new-birthday.com
     BirthdaysComponent,
     NewBirthdayComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/new-birthday/new-birthday.component.ts
+++ src/app/components/new-birthday/new-birthday.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-new-birthday',
   templateUrl: './new-birthday.component.html',
   styleUrls: ['./new-birthday.component.scss'],
 })
-export class NewBirthdayComponent {}
+export class NewBirthdayComponent implements OnInit {
+  newPersonForm: FormGroup;
+  days: number[] = [...Array(31).keys()].map((x: number) => x + 1);
+  months: number[] = [...Array(12).keys()].map((x: number) => x + 1);
+  years: number[] = [];
+  constructor(private fb: FormBuilder) {
+    this.newPersonForm = this.fb.group({
+      firstName: [{ value: null, disabled: false }, Validators.required],
+      lastName: [{ value: null, disabled: false }],
+      day: [{ value: 1, disabled: false }, Validators.required],
+      month: [{ value: 1, disabled: false }, Validators.required],
+      year: [{ value: dayjs().year(), disabled: false }, Validators.required],
+    });
+  }
+
+  ngOnInit() {
+    let currentYear = dayjs().year();
+    for (let i = 0; i < 101; i++) {
+      this.years.push(currentYear);
+      currentYear--;
+    }
+  }
+
+  handleFormSubmit(): void {
+    // todo handle form
+  }
+}
